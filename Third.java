365c8d64f7356da727aa99e40911a0f47d38f295
unknown <srini.pit21@gmail.com>
2021-07-11 13:50:35 +0530

    third commit

---
+++ Third.java
@@ -0,0 +1,9 @@
+public class Third {
+    public static void main(String[] args) {
+        int a[]={10,20,30};
+        for(int i:a)
+        {
+            System.out.println(i);
+        }
+    }
+}
